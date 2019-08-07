0ee4c62075e0a85c7cc6f6c453ed3e5be0bfc593
Your Name <griseldavcardozo@gmail.com>
2019-08-07 15:43:08 -0300

    Updates

---
+++ precache-manifest.67c7ee2e7c5a7c50dcb50768f9179ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d888c11d6b436770538917a8005ae3",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "4d646ebb66c511b4a7a2",
+    "url": "/todo-list-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fcb45d2f816c79b1608e",
+    "url": "/todo-list-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4d646ebb66c511b4a7a2",
+    "url": "/todo-list-react/static/js/2.1d31daf8.chunk.js"
+  },
+  {
+    "revision": "fcb45d2f816c79b1608e",
+    "url": "/todo-list-react/static/js/main.977cb336.chunk.js"
+  },
+  {
+    "revision": "35e3544cd546257cfadf",
+    "url": "/todo-list-react/static/js/runtime~main.bb4d8c92.js"
+  }
+]);
